7ec4a23feccd591992c8e852ae7c7880d9794358
Bruno <synystry1989@gmail.com>
2024-09-05 10:25:58 +0100

    s

--- obj/Debug/net6.0/EchoBot1.AssemblyInfo.cs
+++ obj/Debug/net6.0/EchoBot1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EchoBot1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc735906a95135e8b150dd89670a82866b6102ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a32c31e252098e33dd03a436d96698f3722eaa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("EchoBot1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EchoBot1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
